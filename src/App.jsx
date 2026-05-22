8f81c69aaad07c6663ea67d6e3baddd08b9805c4
Smita <smita16025@gmail.com>
2026-05-22 10:02:43 +0530

    Initialise vite react project with tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen bg-mauve-400 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        How's the Weather?
+      </h1>
+    </div>
+  )
+}
+
+export default App
