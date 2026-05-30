3c51f0bb0bc92ed9dbd348ff973409a361a26c0d
Smita <smita16025@gmail.com>
2026-05-30 22:10:20 +0530

    Add functionality in weathercard

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,29 @@
-function SearchBar() {
+import { useState } from "react";
+
+function SearchBar({ onSearch }) {
+  const [city, setCity] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!city.trim()) return;
+
+    onSearch(city);
+    setCity("");
+  };
+
   return (
-    <div className="w-full flex items-center justify-center">
-      
+    <form
+      className="w-full flex items-center justify-center"
+      onSubmit={(e) => {
+        handleSubmit(e);
+      }}
+    >
       <input
         type="text"
         placeholder="Enter city name"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
         className="h-14 w-1/3 px-10 text-lg text-gray-800 
         border border-gray-300/70 rounded-l-xl
         bg-white/90 backdrop-blur-md
@@ -14,6 +33,7 @@ function SearchBar() {
       />
 
       <button
+        type="submit"
         className="h-14 bg-linear-to-r from-mauve-500 to-mauve-600 text-white px-10 text-lg rounded-r-xl
         shadow-md hover:shadow-xl hover:scale-[1.02]
         active:scale-[0.98]
@@ -22,8 +42,7 @@ function SearchBar() {
       >
         Search
       </button>
-
-    </div>
+    </form>
   );
 }
 
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,7 +1,10 @@
-function WeatherCard() {
+import { WeatherIcon } from "./WeatherIcon";
+
+function WeatherCard({ weather }) {
+
   return (
     <div
-        className="
+      className="
           relative overflow-hidden
           w-full max-w-md
           rounded-3xl
@@ -14,80 +17,84 @@ function WeatherCard() {
           hover:scale-[1.02]
           hover:shadow-[0_25px_70px_-12px_rgba(0,0,0,0.3)]
         "
-      >
-        <div className="absolute -top-16 -right-16 h-40 w-40 rounded-full bg-yellow-300/30 blur-3xl"></div>
-        <div className="absolute -bottom-20 -left-20 h-52 w-52 rounded-full bg-sky-300/20 blur-3xl"></div>
+    >
+      <div className="absolute -top-16 -right-16 h-40 w-40 rounded-full bg-yellow-300/30 blur-3xl"></div>
+      <div className="absolute -bottom-20 -left-20 h-52 w-52 rounded-full bg-sky-300/20 blur-3xl"></div>
 
-        <div className="relative z-10">
-          <h2 className="text-3xl sm:text-4xl font-bold tracking-wide text-gray-900 truncate">
-            City Name
-          </h2>
+      <div className="relative z-10">
+        <h2 className="text-3xl sm:text-4xl font-bold tracking-wide text-gray-900 truncate">
+          {weather.name}, {weather.sys.country}
+        </h2>
 
-          <p className="text-sm sm:text-base text-gray-500 mt-2">
-            Tuesday • 3:45 PM
-          </p>
-        </div>
+        <p className="text-sm sm:text-base text-gray-500 mt-2">
+          {new Date()
+            .toLocaleString("en-US", {
+              weekday: "long",
+              hour: "numeric",
+              minute: "2-digit",
+              hour12: true,
+            })
+            .replace(",", " • ")}
+        </p>
+      </div>
 
-        <div className="relative z-10 flex items-center justify-between mt-8">
-          <div className="flex items-center gap-4">
-            <span className="text-6xl sm:text-7xl drop-shadow-md animate-pulse">
-              ☀️
-            </span>
+      <div className="relative z-10 flex items-center justify-between mt-8">
+        <div className="flex items-center gap-4">
+          <span className="text-6xl sm:text-7xl drop-shadow-md animate-pulse">
+            <WeatherIcon condition={weather.weather[0].main} />
+          </span>
+          <div>
+            <p className="text-5xl sm:text-6xl font-extrabold text-gray-900 leading-none">
+              {weather.main.temp}°
+            </p>
 
-            <div>
-              <p className="text-5xl sm:text-6xl font-extrabold text-gray-900 leading-none">
-                32°
-              </p>
-
-              <p className="text-base sm:text-lg text-gray-500 mt-2 italic">
-                Clear Sky
-              </p>
-            </div>
+            <p className="text-base sm:text-lg text-gray-500 mt-2 italic">
+              {weather.weather[0].description}
+            </p>
           </div>
         </div>
+      </div>
 
-        <div
-          className="
+      <div
+        className="
             relative z-10
             grid grid-cols-3 gap-3 sm:gap-5
             mt-8 pt-6
             border-t border-gray-200/70
           "
-        >
-          <div className="rounded-2xl bg-white/60 p-3 sm:p-4 text-center shadow-sm">
-            <p className="text-[11px] sm:text-xs uppercase tracking-widest text-gray-500">
-              Humidity
-            </p>
+      >
+        <div className="rounded-2xl bg-white/60 p-3 sm:p-4 text-center shadow-sm">
+          <p className="text-[11px] sm:text-xs uppercase tracking-widest text-gray-500 pt-2">
+            Humidity
+          </p>
 
-            <p className="text-2xl sm:text-3xl font-bold text-gray-800 mt-2">
-              60%
-            </p>
-          </div>
+          <p className="text-2xl sm:text-3xl font-bold text-gray-800 mt-2">
+            {weather.main.humidity}%
+          </p>
+        </div>
 
-          <div className="rounded-2xl bg-white/60 p-3 sm:p-4 text-center shadow-sm">
-            <p className="text-[11px] sm:text-xs uppercase tracking-widest text-gray-500">
-              Wind
-            </p>
+        <div className="rounded-2xl bg-white/60 p-3 sm:p-4 text-center shadow-sm">
+          <p className="text-[11px] sm:text-xs uppercase tracking-widest text-gray-500 pt-2">
+            Wind
+          </p>
 
-            <p className="text-2xl sm:text-3xl font-bold text-gray-800 mt-2">
-              12
-              <span className="text-sm sm:text-base ml-1 font-medium">
-                km/h
-              </span>
-            </p>
-          </div>
+          <p className="text-2xl sm:text-3xl font-bold text-gray-800 mt-2 flex items-end justify-center flex-wrap">
+            {weather.wind.speed}
+            <span className="text-sm sm:text-base ml-1 font-medium">km/h</span>
+          </p>
+        </div>
 
-          <div className="rounded-2xl bg-white/60 p-3 sm:p-4 text-center shadow-sm">
-            <p className="text-[11px] sm:text-xs uppercase tracking-widest text-gray-500 whitespace-nowrap">
-              Feels Like
-            </p>
+        <div className="rounded-2xl bg-white/60 p-3 sm:p-4 text-center shadow-sm">
+          <p className="text-[11px] sm:text-xs uppercase tracking-widest text-gray-500 whitespace-nowrap pt-2">
+            Feels Like
+          </p>
 
-            <p className="text-2xl sm:text-3xl font-bold text-gray-800 mt-2">
-              35°
-            </p>
-          </div>
+          <p className="text-2xl sm:text-3xl font-bold text-gray-800 mt-2">
+            {weather.main.feels_like}°
+          </p>
         </div>
       </div>
+    </div>
   );
 }
 
